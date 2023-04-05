d3e2bd95b612b14276b2735c6a97230fa595da15
Shivam <balmikishivam870@gmail.com>
2023-04-05 17:21:29 +0530

    Breakout Game - Basic
    Grid + Tiles

---
+++ Simple Games/Breakout Game/script.js
@@ -0,0 +1,46 @@
+const grid = document.querySelector(".grid");
+const blockWidth = 100;
+const blockHeight = 20;
+
+// Create Block
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis];
+        this.bottomRight = [xAxis + blockWidth, yAxis];
+        this.topLeft = [xAxis, yAxis + blockHeight];
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+    }
+}
+
+// All my blocks
+const blocks = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+];
+
+console.log(blocks[0]);
+
+// Draw all my block
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement("div");
+        block.classList.add("block");
+        block.style.left = blocks[i].bottomLeft[0] + "px";
+        block.style.bottom = blocks[i].bottomLeft[1] + "px";
+        grid.appendChild(block);
+    }
+}
+addBlocks();
