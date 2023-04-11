3e95b656aa59ab3e5c049edbdc97cd75796edfee
Shivam <balmikishivam870@gmail.com>
2023-04-11 13:12:04 +0530

    Structure made

--- Simple Games/Connect Four/script.js
+++ Simple Games/Connect Four/script.js
@@ -1,4 +1,15 @@
 // document.addEventListener('DOMContentLoaded', () => {
-    // When script tag is in heading without defer
+// When script tag is in heading without defer
 // })
 
+const squares = document.querySelectorAll(".grid div");
+const result = document.querySelector("#result");
+const displayCurrentPlayer = document.querySelector("#current-player");
+
+let currentPlayer = 1;
+
+for (let i = 0; i < squares.length; i++) {
+    squares[i].onclick = () => {
+        alert("You have clicked Square" + i);
+    };
+}
