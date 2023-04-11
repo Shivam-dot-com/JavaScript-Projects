d5b6db50e1857a8bc412965dd68208edcbb8b1df
Shivam <balmikishivam870@gmail.com>
2023-04-11 13:34:22 +0530

    Game Logic

--- Simple Games/Connect Four/script.js
+++ Simple Games/Connect Four/script.js
@@ -10,6 +10,21 @@ let currentPlayer = 1;
 
 for (let i = 0; i < squares.length; i++) {
     squares[i].onclick = () => {
-        alert("You have clicked Square" + i);
+        // If the square below your current square is taken, you can go on top of it
+        if (squares[i + 7].classList.contains("taken")) {
+            if (currentPlayer == 1) {
+                squares[i].classList.add("taken");
+                squares[i].classList.add("player-one");
+                currentPlayer = 2;
+                displayCurrentPlayer.innerHTML = currentPlayer;
+            } else if (currentPlayer == 2) {
+                squares[i].classList.add("taken");
+                squares[i].classList.add("player-two");
+                currentPlayer = 1;
+                displayCurrentPlayer.innerHTML = currentPlayer;
+            }
+        } else {
+            alert("Can't go here");
+        }
     };
 }
