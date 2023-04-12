2e11c97660549ed6b26d648ff58488b7a3f47171
Shivam <balmikishivam870@gmail.com>
2023-04-12 13:49:23 +0530

    Game Finished

--- Simple Games/Space Invaders/script.js
+++ Simple Games/Space Invaders/script.js
@@ -1,5 +1,12 @@
 const grid = document.querySelector(".grid");
+const resultDisplay = document.querySelector(".result");
 let currentShooterIndex = 202;
+let width = 15;
+let direction = 1;
+let invadersId;
+let aliensRemoved = []
+let result = 0;
+let goingRight = true;
 
 for (let i = 0; i < 255; i++) {
     const square = document.createElement("div");
@@ -15,9 +22,110 @@ const alienInvaders = [
 
 function draw() {
     for (let i = 0; i < alienInvaders.length; i++) {
-        squares[alienInvaders[i]].classList.add("invader");
+        if (!aliensRemoved.includes(i)) { 
+            squares[alienInvaders[i]].classList.add("invader");
+        }
     }
 }
 draw();
 
+function remove() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.remove("invader");
+    }
+}
+
 squares[currentShooterIndex].classList.add("shooter");
+
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove("shooter");
+    switch (e.key) {
+        case "ArrowLeft":
+            if (currentShooterIndex % width !== 0) currentShooterIndex -= 1;
+            break;
+        case "ArrowRight":
+            if (currentShooterIndex % width < width - 1)
+                currentShooterIndex += 1;
+            break;
+    }
+    squares[currentShooterIndex].classList.add("shooter");
+}
+document.addEventListener("keydown", moveShooter);
+
+function moveInvaders() {
+    const leftEdge = alienInvaders[0] % width === 0;
+    const rightEdge =
+        alienInvaders[alienInvaders.length - 1] % width === width - 1;
+    remove();
+
+    // RightEdge
+    if (rightEdge && goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width + 1;
+            direction = -1;
+            goingRight = false;
+        }
+    }
+    if (leftEdge && !goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width - 1;
+            direction = 1;
+            goingRight = true;
+        }
+    }
+
+    for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += direction;
+    }
+    draw();
+
+    if (squares[currentShooterIndex].classList.contains("invader", "shooter")) {
+        resultDisplay.innerHTML = "Game Over";
+        clearInterval(invadersId);
+    }
+    console.log(squares.length);
+    for (let i = 0; i < alienInvaders.length; i++) {
+        console.log(alienInvaders[i]);
+        if (alienInvaders[i] > squares.length) {
+            resultDisplay.innerHTML = "Game Over";
+            clearInterval(invadersId);
+        }
+    }
+    if (aliensRemoved.length === alienInvaders.length) { 
+        resultDisplay.innerHTML = "You Win"
+        clearInterval(invadersId)
+    }
+}
+
+invadersId = setInterval(moveInvaders, 300);
+
+function shoot(e) {
+    let laserId;
+    let currentLaserIndex = currentShooterIndex;
+    function moveLaser() {
+        squares[currentLaserIndex].classList.remove('laser')
+        currentLaserIndex -= width  
+        squares[currentLaserIndex].classList.add('laser')
+        
+        if (squares[currentLaserIndex].classList.contains('invader')) {         
+            squares[currentLaserIndex].classList.remove('laser')
+            squares[currentLaserIndex].classList.remove('invader')
+            squares[currentLaserIndex].classList.add('boom')
+
+            setTimeout(() => squares[currentLaserIndex].classList.remove('boom'), 300)
+            clearInterval(laserId)
+            
+            const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+            aliensRemoved.push(alienRemoved)
+            result++;
+            resultDisplay.innerHTML = result
+            console.log(aliensRemoved)
+        }   
+    }
+    switch (e.key) { 
+        case "ArrowUp":
+            laserId = setInterval(moveLaser,100)
+    }
+}
+
+document.addEventListener('keydown',shoot)
