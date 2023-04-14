c3d1fa1b9a915a8abcabe5dc1a54e647f71a7752
Shivam <balmikishivam870@gmail.com>
2023-04-14 10:04:23 +0530

    API Key set

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,3 +1,13 @@
 let weather = {
-    "apiKey":"78c0b2346ea6f1c362d30f77baca2be6"
-}       
+    apiKey: "78c0b2346ea6f1c362d30f77baca2be6",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=metric&appid=" +
+                this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+};
