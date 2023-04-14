8feaf9fca26d5b5a24f8e0ee818666c346c63252
Shivam <balmikishivam870@gmail.com>
2023-04-14 10:48:26 +0530

    Working

--- Weather App/script.js
+++ Weather App/script.js
@@ -15,13 +15,30 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
+        // console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").textContent = "Weather in " + name;
         document.querySelector(".icon").src =
             "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").textContent = description;
-        document.querySelector(".temp").textContent = temp + "°C"; 
-        document.querySelector(".humidity").textContent = "Humidity: " + humidity + "%";        
-        document.querySelector(".wind").textContent = "Wind Speed: " +  speed + "km/h"
+        document.querySelector(".temp").textContent = temp + "°C";
+        document.querySelector(".humidity").textContent =
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").textContent =
+            "Wind Speed: " + speed + "km/h";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     },
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
