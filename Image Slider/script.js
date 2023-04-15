5095d27957a0a78311111c09635f870ad31bd607
Shivam <balmikishivam870@gmail.com>
2023-04-15 16:07:43 +0530

    Buttons Working

--- Image Slider/script.js
+++ Image Slider/script.js
@@ -1,1 +1,16 @@
-console.log("Working")
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll('.image')
+
+let slideNumber = 1;
+
+right.addEventListener("click", () => {
+    if (slideNumber < images.length) {
+        slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+        slideNumber++;
+    } else { 
+        slider.style.transform = `translateX(0px)`;
+        slideNumber = 1;
+    }
+});
