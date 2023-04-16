e662473ae1e1722734176913f2b9d95c582500bc
Shivam <balmikishivam870@gmail.com>
2023-04-16 14:34:19 +0530

    Update Script

---
+++ Product Filter/script.js
@@ -0,0 +1,108 @@
+const data = [
+    {
+        id: 1,
+        name: "Invicta Men's Pro Diver",
+        img: "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
+        price: 74,
+        cat: "Dress",
+    },
+    {
+        id: 11,
+        name: "Invicta Men's Pro Diver 2",
+        img: "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
+        price: 74,
+        cat: "Dress",
+    },
+    {
+        id: 2,
+        name: "Timex Men's Expedition Scout ",
+        img: "https://m.media-amazon.com/images/I/91WvnZ1g40L._AC_UY879_.jpg",
+        price: 40,
+        cat: "Sport",
+    },
+    {
+        id: 3,
+        name: "Breitling Superocean Heritage",
+        img: "https://m.media-amazon.com/images/I/61hGDiWBU8L._AC_UY879_.jpg",
+        price: 200,
+        cat: "Luxury",
+    },
+    {
+        id: 4,
+        name: "Casio Classic Resin Strap ",
+        img: "https://m.media-amazon.com/images/I/51Nk5SEBARL._AC_UY879_.jpg",
+        price: 16,
+        cat: "Sport",
+    },
+    {
+        id: 5,
+        name: "Garmin Venu Smartwatch ",
+        img: "https://m.media-amazon.com/images/I/51kyjYuOZhL._AC_SL1000_.jpg",
+        price: 74,
+        cat: "Casual",
+    },
+];
+
+const productsContainer = document.querySelector(".products");
+const searchInput = document.querySelector(".search");
+const categoriesContainer = document.querySelector(".cats");
+const priceRange = document.querySelector(".priceRange");
+const priceValue = document.querySelector(".priceValue");
+
+const displayProducts = (filteredProducts) => {
+    productsContainer.innerHTML = filteredProducts.map(
+        (product) =>
+            `
+        <div class="product">
+            <img src="${product.img}" alt="Image" />
+            <span class="name">${product.name}</span>
+            <span class="priceText">$${product.price}</span>
+        </div>
+        `
+    );
+};
+
+displayProducts(data);
+
+// Searching
+searchInput.addEventListener("keyup", (e) => {
+    const value = e.target.value.toLowerCase();
+
+    if (value) {
+        // Filter Prodcuts
+        displayProducts(
+            data.filter((item) => item.name.toLowerCase().indexOf(value) !== -1)
+        );
+    } else {
+        // Show Products
+        displayProducts(data);
+    }
+});
+
+// Categories
+const setCategories = () => {
+    const allCats = data.map((item) => item.cat);
+    const categories = [
+        "All",
+        ...allCats.filter((item, i) => {
+            return allCats.indexOf(item) === i;
+        }),
+    ];
+    categoriesContainer.innerHTML = categories
+        .map(
+            (cat) =>
+                `   
+        <span class="cats">${cat}</span>
+        `
+        )
+        .join("");
+
+    categoriesContainer.addEventListener("click", (e) => {
+        const selectedCat = e.target.textContent;
+
+        selectedCat === "All"
+            ? displayProducts(data)
+            : displayProducts(data.filter((item) => item.cat === selectedCat));
+    });
+};
+setCategories();
