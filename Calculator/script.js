ba531b3aebee68d1da646b1c6e6f57700d0b4d02
Shivam <balmikishivam870@gmail.com>
2023-04-20 21:06:44 +0530

    Script Added

---
+++ Calculator/script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelector('.buttons')
+
+// Function buttons
+const clear = document.querySelector('.clear')
+const negative = document.querySelector('.negative')
+const percent = document.querySelector('.percent')
+
+// Operators
+const divisionBtn = document.querySelector('.item3')
+const multiplyBtn = document.querySelector('.item3')
+const addBtn = document.querySelector('.item3')
+const subtractBtn = document.querySelector('.item3')
+const comma = document.querySelector('.comma')
+const Equals = document.querySelector('.equals')
+
+clear.addEventListener("click", () => { 
+    console.log("Clear is pressed")
+})
+
