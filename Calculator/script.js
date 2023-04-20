4e19ec29e68e74ead20560676fbbea3c5d0419aa
Shivam <balmikishivam870@gmail.com>
2023-04-20 21:18:15 +0530

    Operations added

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,19 +1,35 @@
-const buttons = document.querySelector('.buttons')
+const buttons = document.querySelector(".buttons");
+let operand_1;
+let operand_2;
 
 // Function buttons
-const clear = document.querySelector('.clear')
-const negative = document.querySelector('.negative')
-const percent = document.querySelector('.percent')
+const clear = document.querySelector(".clear");
+const negative = document.querySelector(".negative");
+const percent = document.querySelector(".percent");
 
 // Operators
-const divisionBtn = document.querySelector('.item3')
-const multiplyBtn = document.querySelector('.item3')
-const addBtn = document.querySelector('.item3')
-const subtractBtn = document.querySelector('.item3')
-const comma = document.querySelector('.comma')
-const Equals = document.querySelector('.equals')
+const divisionBtn = document.querySelector(".item3");
+const multiplyBtn = document.querySelector(".item3");
+const addBtn = document.querySelector(".item3");
+const subtractBtn = document.querySelector(".item3");
+const comma = document.querySelector(".comma");
+const Equals = document.querySelector(".equals");
 
-clear.addEventListener("click", () => { 
-    console.log("Clear is pressed")
-})
+clear.addEventListener("click", () => {
+    console.log("Clear is pressed");
+});
+
+const addFunction = () => {
+    console.log(operand_1 + operand_2)
+};
+const subtractFunction = () => {
+    console.log(operand_1 - operand_2)
+};
+const divisionFunction = () => {
+    console.log(operand_1 / operand_2)
+};
+const multiplyFunction = () => {
+    console.log(operand_1 * operand_2)
+};
+
 
