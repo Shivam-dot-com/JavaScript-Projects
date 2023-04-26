36d153b10cc723d5dbfd7fc952a7bb8c716702a4
Shivam <balmikishivam870@gmail.com>
2023-04-26 12:54:43 +0530

    Traversing throught DOM

--- General Questions/script.js
+++ General Questions/script.js
@@ -1,2 +1,13 @@
 // Using selector inside the element
-// Traversing the DOM
+
+//* Traversing the DOM : Fancy way of moving up & down of DOM Tree
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const question = e.currentTarget.parentElement.parentElement
+        question.classList.toggle('show-text')
+    });
+});
+
+// We used parentElement here, for accessing parent of selected element, same way you can choose child and siblings. 
