ebd8830a55965ed1bad036a32075099385460442
Shivam <balmikishivam870@gmail.com>
2023-04-26 13:05:58 +0530

    Using Selector

--- General Questions/script.js
+++ General Questions/script.js
@@ -1,13 +1,23 @@
 // Using selector inside the element
-
-//* Traversing the DOM : Fancy way of moving up & down of DOM Tree
-const btns = document.querySelectorAll(".question-btn");
-
-btns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        const question = e.currentTarget.parentElement.parentElement
-        question.classList.toggle('show-text')
+const questions = document.querySelectorAll(".question");
+questions.forEach((question) => {
+    // console.log(`🚀 ~ file: script.js:5 ~ questions.forEach ~ question:`, question)
+    const btn = question.querySelector(".question-btn");
+    //! Observe with querySelector we have used question
+    // console.log(`🚀 ~ file: script.js:6 ~ questions.forEach ~ btn:`, btn)
+    btn.addEventListener("click", () => {
+        question.classList.toggle("show-text");
     });
 });
 
-// We used parentElement here, for accessing parent of selected element, same way you can choose child and siblings. 
+//* Traversing the DOM : Fancy way of moving up & down of DOM Tree
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+//     btn.addEventListener("click", (e) => {
+//         const question = e.currentTarget.parentElement.parentElement
+//         question.classList.toggle('show-text')
+//     });
+// });
+
+// We used parentElement here, for accessing parent of selected element, same way you can choose child and siblings.
