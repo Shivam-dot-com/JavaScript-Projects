7b2ca3cdcc4e7d0d062bbb9fb251a05982b7c2ae
Shivam <balmikishivam870@gmail.com>
2023-04-27 21:04:23 +0530

    Video Pause & Play

--- Video Project/script.js
+++ Video Project/script.js
@@ -2,14 +2,28 @@
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and sub frames to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 
-const btn = document.querySelector('.switch-btn')
-const video = document.querySelector('.video-container')
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
 
-btn.addEventListener('click', () => { 
-    if (!btn.classList.contains('slide')) {  //*Focus on not operator here 
-        btn.classList.add('slide')
+btn.addEventListener("click", () => {
+    if (!btn.classList.contains("slide")) {
+        //*Focus on not operator here
+        btn.classList.add("slide");
+        video.pause();
+        video.controls = "false"
+    } else {
+        btn.classList.remove("slide");
+        video.play();
     }
-    else { 
-        btn.classList.remove('slide')
-    }
+});
+
+// *Video.play() & Video.pause() will make it to maintain with btn
+
+/* Adding Pre-loader functionality, using Load Event, it fired when the whole page is loaded best use for Preloader */
+// * The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const preloader = document.querySelector('.preloader')
+
+window.addEventListener('load', () => { 
+    preloader.classList.add('hide-preloader')
 })
